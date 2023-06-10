27da9af7d29da515c5ce4bb1480e8ed7311de3d2
Donghoon <thdehdgns@gmail.com>
2023-06-10 16:02:26 +0900

    feat: UserInfo type 추가

--- src/atoms/auth.ts
+++ src/atoms/auth.ts
@@ -1,6 +1,7 @@
+import { UserInfo } from "@/types/auth";
 import { atom } from "recoil";
 
-export const authState = atom({
+export const authState = atom<UserInfo>({
   key: "authState", // unique ID (with respect to other atoms/selectors)
   default: null, // default value (aka initial value)
 });
