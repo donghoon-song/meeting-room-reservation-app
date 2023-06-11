cffd4acdd2b7a45e7d826749e59e01dfcdfe1192
Donghoon <thdehdgns@gmail.com>
2023-06-11 11:37:06 +0900

    feat: RoomCalendar 추가 & reservation data 연동

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,37 +1,14 @@
 "use client";
 
-import { Calendar, dayjsLocalizer } from "react-big-calendar";
-import dayjs from "dayjs";
-import "dayjs/locale/ko";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-
-dayjs.locale("ko");
-const localizer = dayjsLocalizer(dayjs);
-
-const events = [
-  {
-    start: dayjs().toDate(),
-    end: dayjs().add(1, "day").toDate(),
-    title: "Some title",
-  },
-];
-
-const MyCalendar = () => (
-  <div>
-    <Calendar
-      localizer={localizer}
-      events={events}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 500 }}
-    />
-  </div>
-);
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useEffect, useState } from "react";
+import { Reservation } from "../../../types/reservation";
+import Calendar from "@/components/RoomCalendar";
 
 export default function CalendarPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <MyCalendar />
+      <Calendar />
     </main>
   );
 }
