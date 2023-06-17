819b74cc37ea9cfdefe6f27f10f3b99446eb6987
Donghoon <thdehdgns@gmail.com>
2023-06-17 23:38:18 +0900

    fix: Modal overflow hidden 제거
    timePicker가 제대로 안 보임

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function Modal({ children }: Props) {
   return (
     <dialog id="my_modal_1" className="modal">
-      <div className="modal-box">
+      <div className="modal-box overflow-visible">
         {/* <form method="dialog" className="modal-box"> */}
         <>{children}</>
         {/* </form> */}
