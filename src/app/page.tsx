33ab84b846ed6d4706e0f9da2809099e18a07e15
Donghoon <thdehdgns@gmail.com>
2023-06-22 22:16:54 +0900

    feat: 로그인 상태면 캘린더 창으로 redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+
+import { useRecoilValue } from "recoil";
 import Login from "./login";
+import { getUserInfo } from "@/atoms/auth";
+import { UserInfo } from "@/types/auth";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const userInfo = useRecoilValue<UserInfo>(getUserInfo);
+  if (userInfo) {
+    redirect("/calendar");
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Login />
