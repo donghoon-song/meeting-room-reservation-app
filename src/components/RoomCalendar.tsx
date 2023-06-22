ca160090b1b4bf1f87bf12e142bab75a9dc08541
Donghoon <thdehdgns@gmail.com>
2023-06-22 23:46:17 +0900

    feat: 회의실 인원, 예약자 이름 정보 추가

--- src/components/RoomCalendar.tsx
+++ src/components/RoomCalendar.tsx
@@ -18,7 +18,9 @@ export default function RoomCalendar() {
       // TODO : 가져올 때  Event type으로 가져올 순 없을까?
       const { data, error } = await supabase
         .from("reservations")
-        .select("id, start_time, end_time, rooms ( name )");
+        .select(
+          "id, start_time, end_time, rooms ( name, capacity ), users ( name )"
+        );
       if (data) {
         setEvents(
           data.map((reservation) => {
@@ -26,7 +28,13 @@ export default function RoomCalendar() {
               id: reservation.id,
               start: new Date(reservation.start_time),
               end: new Date(reservation.end_time),
-              title: reservation.rooms.name,
+              rooms: {
+                name: reservation.rooms.name,
+                capacity: reservation.rooms.capacity,
+              },
+              users: {
+                name: reservation.users.name,
+              },
             };
           })
         );
