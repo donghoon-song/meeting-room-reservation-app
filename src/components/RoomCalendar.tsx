18b5c475a6798de0f7b9202d646db7f2ea53ced9
Donghoon <thdehdgns@gmail.com>
2023-06-27 22:27:54 +0900

    feat: 캘린더에서 내부회의실 예약 내역 backgroundColor 변경

--- src/components/RoomCalendar.tsx
+++ src/components/RoomCalendar.tsx
@@ -2,6 +2,7 @@ import { Calendar, dayjsLocalizer } from "react-big-calendar";
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useCallback } from "react";
 
 dayjs.locale("ko");
 const localizer = dayjsLocalizer(dayjs);
@@ -13,6 +14,18 @@ type Props = {
 };
 
 export default function RoomCalendar({ events, onSelectEvent }: Props) {
+  // TODO: any 없애기
+  const eventPropGetter = useCallback(
+    (event: any) => ({
+      ...(event?.rooms.name.includes("내부") && {
+        style: {
+          backgroundColor: "#31ada8",
+        },
+      }),
+    }),
+    []
+  );
+
   return (
     <Calendar
       localizer={localizer}
@@ -22,6 +35,7 @@ export default function RoomCalendar({ events, onSelectEvent }: Props) {
       defaultView="week"
       views={["month", "week", "day"]}
       style={{ width: "100%", height: 500 }}
+      eventPropGetter={eventPropGetter}
       onSelectEvent={onSelectEvent}
     />
   );
