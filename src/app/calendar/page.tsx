da7fb8948df1bab9a08307d7b34db3a8547083fc
Donghoon <thdehdgns@gmail.com>
2023-06-27 22:18:24 +0900

    feat: useReservations 추가 & 예약 성공 후에 fetchReservations

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -7,11 +7,11 @@ import RoomCalendar from "@/components/RoomCalendar";
 import ReservationModal from "@/components/ReservationModal";
 import useModal from "@/hooks/useModal";
 import ReservationDetailModal from "@/components/ReservationDetailModal";
-import { Event } from "react-big-calendar";
+import useReservations from "@/hooks/useReservations";
 
 export default function CalendarPage() {
   const supabase = createClientComponentClient();
-  const [events, setEvents] = useState<Event[]>([]);
+  const [events, fetchReservations] = useReservations();
 
   const [isReservationModalOpen, openReservationModal, closeReservationModal] =
     useModal();
@@ -23,34 +23,8 @@ export default function CalendarPage() {
   const [reservationDetail, setReservationDetail] = useState<Reservation>();
 
   useEffect(() => {
-    const fetchReservations = async () => {
-      // TODO : 가져올 때  Event type으로 가져올 순 없을까?
-      const { data, error } = await supabase
-        .from("reservations")
-        .select(
-          "id, start_time, end_time, rooms ( name, capacity ), users ( name )"
-        );
-      if (data) {
-        setEvents(
-          data.map((reservation) => {
-            return {
-              id: reservation.id,
-              start: new Date(reservation.start_time),
-              end: new Date(reservation.end_time),
-              rooms: {
-                name: reservation.rooms.name,
-                capacity: reservation.rooms.capacity,
-              },
-              users: {
-                name: reservation.users.name,
-              },
-            };
-          })
-        );
-      }
-    };
     fetchReservations();
-  }, [setEvents, supabase]);
+  }, [fetchReservations]);
 
   // TODO: any type 제거하기
   const handleSelectEvent = (
@@ -72,7 +46,10 @@ export default function CalendarPage() {
       </button>
       <ReservationModal
         open={isReservationModalOpen}
-        onSuccess={() => closeReservationModal()}
+        onSuccess={() => {
+          closeReservationModal();
+          fetchReservations();
+        }}
         onCancel={() => closeReservationModal()}
       />
       <ReservationDetailModal
