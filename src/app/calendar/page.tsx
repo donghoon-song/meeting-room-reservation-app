86031154a1f2b14246295d6bd89b64b58582b786
Donghoon <thdehdgns@gmail.com>
2023-06-27 22:09:36 +0900

    refactor: fetchReservations page로 이동

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -7,8 +7,12 @@ import RoomCalendar from "@/components/RoomCalendar";
 import ReservationModal from "@/components/ReservationModal";
 import useModal from "@/hooks/useModal";
 import ReservationDetailModal from "@/components/ReservationDetailModal";
+import { Event } from "react-big-calendar";
 
 export default function CalendarPage() {
+  const supabase = createClientComponentClient();
+  const [events, setEvents] = useState<Event[]>([]);
+
   const [isReservationModalOpen, openReservationModal, closeReservationModal] =
     useModal();
   const [
@@ -17,6 +21,36 @@ export default function CalendarPage() {
     closeReservationDetailModal,
   ] = useModal();
   const [reservationDetail, setReservationDetail] = useState<Reservation>();
+
+  useEffect(() => {
+    const fetchReservations = async () => {
+      // TODO : 가져올 때  Event type으로 가져올 순 없을까?
+      const { data, error } = await supabase
+        .from("reservations")
+        .select(
+          "id, start_time, end_time, rooms ( name, capacity ), users ( name )"
+        );
+      if (data) {
+        setEvents(
+          data.map((reservation) => {
+            return {
+              id: reservation.id,
+              start: new Date(reservation.start_time),
+              end: new Date(reservation.end_time),
+              rooms: {
+                name: reservation.rooms.name,
+                capacity: reservation.rooms.capacity,
+              },
+              users: {
+                name: reservation.users.name,
+              },
+            };
+          })
+        );
+      }
+    };
+    fetchReservations();
+  }, [setEvents, supabase]);
 
   // TODO: any type 제거하기
   const handleSelectEvent = (
@@ -29,7 +63,7 @@ export default function CalendarPage() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-4 sm:p-24 ">
-      <RoomCalendar onSelectEvent={handleSelectEvent} />
+      <RoomCalendar events={events} onSelectEvent={handleSelectEvent} />
       <button
         className="primary-button w-full h-10 mt-6"
         onClick={() => openReservationModal()}
--- src/components/RoomCalendar.tsx
+++ src/components/RoomCalendar.tsx
@@ -1,53 +1,18 @@
-import { Calendar, Event, dayjsLocalizer } from "react-big-calendar";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { SyntheticEvent, useEffect, useState } from "react";
+import { Calendar, dayjsLocalizer } from "react-big-calendar";
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { Reservation } from "../../types/reservation";
 
 dayjs.locale("ko");
 const localizer = dayjsLocalizer(dayjs);
 
 type Props = {
+  events: any[];
   // todo : Tevent type으로 바꾸기
   onSelectEvent?: (event: any, e: React.SyntheticEvent<HTMLElement>) => void;
 };
 
-export default function RoomCalendar({ onSelectEvent }: Props) {
-  const supabase = createClientComponentClient();
-  const [events, setEvents] = useState<Event[]>([]);
-
-  useEffect(() => {
-    const fetchReservations = async () => {
-      // TODO : 가져올 때  Event type으로 가져올 순 없을까?
-      const { data, error } = await supabase
-        .from("reservations")
-        .select(
-          "id, start_time, end_time, rooms ( name, capacity ), users ( name )"
-        );
-      if (data) {
-        setEvents(
-          data.map((reservation) => {
-            return {
-              id: reservation.id,
-              start: new Date(reservation.start_time),
-              end: new Date(reservation.end_time),
-              rooms: {
-                name: reservation.rooms.name,
-                capacity: reservation.rooms.capacity,
-              },
-              users: {
-                name: reservation.users.name,
-              },
-            };
-          })
-        );
-      }
-    };
-    fetchReservations();
-  }, [setEvents, supabase]);
-
+export default function RoomCalendar({ events, onSelectEvent }: Props) {
   return (
     <Calendar
       localizer={localizer}
