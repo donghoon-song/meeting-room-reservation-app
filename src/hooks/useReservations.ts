baed2adac5507b25bf87a07c674c0c8f56a40155
Donghoon <thdehdgns@gmail.com>
2023-06-28 23:55:02 +0900

    fix: type error

--- src/hooks/useReservations.ts
+++ src/hooks/useReservations.ts
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Event } from "react-big-calendar";
+import { Database } from "../../types/supabase";
 
 const useReservations = (): [Event[], () => void] => {
   const [events, setEvents] = useState<Event[]>([]);
-  const supabase = createClientComponentClient();
+  const supabase = createClientComponentClient<Database>();
 
   const fetchReservations = async () => {
     // TODO : 가져올 때  Event type으로 가져올 순 없을까?
@@ -21,11 +22,11 @@ const useReservations = (): [Event[], () => void] => {
             start: new Date(reservation.start_time),
             end: new Date(reservation.end_time),
             rooms: {
-              name: reservation.rooms.name,
-              capacity: reservation.rooms.capacity,
+              name: reservation.rooms ? reservation.rooms.name : "",
+              capacity: reservation.rooms ? reservation.rooms.capacity : "",
             },
             users: {
-              name: reservation.users.name,
+              name: reservation.users ? reservation.users.name : "",
             },
           };
         })
