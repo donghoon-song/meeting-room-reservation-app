87ad7d063adbc503e2d61ec2b99f53991dd85195
Donghoon <thdehdgns@gmail.com>
2023-06-29 00:23:05 +0900

    feat: 메인화면 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Login from "./login";
 import { getUserInfo } from "@/atoms/auth";
 import { UserInfo } from "@/types/auth";
 import { redirect } from "next/navigation";
+import Image from "next/image";
 
 export default function Home() {
   const userInfo = useRecoilValue<UserInfo>(getUserInfo);
@@ -13,7 +14,15 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-20">
+      <h1 className="text-2xl text-black mb-4">코멘토 회의실 예약 시스템</h1>
+      <Image
+        src="https://olpvsxmuscjzgohdzmsy.supabase.co/storage/v1/object/public/public/main-image.png"
+        width={500}
+        height={500}
+        className="rounded-lg mb-4"
+        alt="회의실 이미지"
+      />
       <Login />
     </main>
   );
